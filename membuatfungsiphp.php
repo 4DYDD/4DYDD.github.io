c2a6c250c868875434bcdddb491e50bc77fb00e3
4DYDD <111673708+4DYDD@users.noreply.github.com>
2022-08-22 12:03:27 +0800

    Add files via upload

---
+++ membuatfungsiphp.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>function php</title>
+	<style type="text/css">
+		body {
+			text-align: center;
+			color: black;
+			font-family: Berlin Sans FB;
+		}
+	</style>
+</head>
+<body>
+
+<?php
+
+function perkenalan($nama, $ucap){
+	echo "~~~~".$ucap."~~~~<br>";
+	echo "perkenalkan nama saya ".$nama."<br>";
+	echo "Senang berkenalan dengan Anda<br>";
+	echo "pada hari ini kita akan mengadakan ceramah singkat, bagaimana cara agar kita menjadi pribadi yang baik<br>";
+	echo "pertama :<br>";
+	echo "Rajin beribadah, dengan rajin beribadah kita akan selalu merasa tenang dan dengan beribadah kita akan selalu mengingat Allah, dengan mengingat Allah akan membuat kita terus takut berbuat salah karena kita tau Allah maha Melihat, dan kita malu kepada Allah, kita jadi terjauhkan dari maksiat dan perbuatan buruk diwaktu sendiri, kita jadi pribadi yang penyabar dan tidak mudah marah karena dengan ingat kepada Allah kita tau kalau Allah maha Kuasa dan Maha Perkasa, dengan mengingat Allah hati menjadi tenang dan diajuhkan dari hal2 buruk.<br>";
+	echo "kedua :<br>";
+	echo "Rajin bersedekah, dengan itu kita bisa terhindarkan dari sifat kikir dan tamak, serta sifat sombong, dengan bersedekah hati menjadi senang, melihat orang bahagia, harta bukan segalanya, harta adalah perantara kita untuk mendapatkan semua barang dunia, akan tetapi dengan bersedekah kita menjadikan harta juga bermanfaat bagi orang lain, dan menjauhkan kita dari sifat foya-foya.<br>";
+
+}
+perkenalan("adit","hai");
+
+echo "<br>";
+?>
+
+
+</body>
+</html>
---
+++ membuatfungsiphppart2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		p {
+			color: black;
+			font-family: Berlin Sans FB;
+			text-align: center;
+			background: floralwhite;
+			size: 40px;
+		}
+		
+		a {
+			text-decoration: none;
+			font-family: Berlin Sans FB;
+    		font-size: 15px;
+    		background: #FFE4C4;
+    		color: white;
+    		border: white 3px solid;
+    		border-radius: 5px;
+    		padding: 12px 20px;
+    		margin-top: 10px;
+    		margin-right: 10px;
+    		margin-left: 10px;
+		}
+	</style>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>function php</title>
+</head>
+<body>
+
+<p>Part 2 belum tersedia tolong menunggu sampai tanggal 23/08/2022 ~~~terimakasih~~~
+</br><img src="anjingmintamaaf.png" alt="inistikerkamal" align="middle" height="100" width="200" /></p></br></br>
+
+<div align="middle">
+		<a href="https://4DYDD.github.io/membuatfungsiphp.php"> part1 </a>
+		<a href="https://4DYDD.github.io/membuatfungsiphppart2.php"> part2 </a>
+	</div>
+</body>
+</html>
