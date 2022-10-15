81695cf09666a330091678d4949113f7c18bb7bd
4DYDD <111673708+4DYDD@users.noreply.github.com>
2022-10-15 11:33:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function tombol (pilihan){
+
+	let NILAI = document.getElementById('POIN');
+	 if (pilihan == "A") {
+	 	alert("JAWABAN ANDA BENAR");
+	 	document.getElementById('POIN').innerHTML=10;
+	 }
+
+	 if (pilihan == "B") {
+	 	alert("JAWABAN ANDA SALAH");
+	 	
+	 }
+	 
+} 
