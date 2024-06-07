ff9bb837f8784e58136ec9bc819e43be6d520bba
4DYDD <111673708+4DYDD@users.noreply.github.com>
2024-06-07 10:52:46 +0800

    Create samting.php

---
+++ samting.php
@@ -0,0 +1,9 @@
+<?php
+$data = [
+  ["nama" => "dobleh", "keahlian" => "jago bunshin"],
+  ["nama" => "amat", "keahlian" => "sepuh jamping"],
+  ["nama" => "jundin", "keahlian" => "ngendok di tempat terbuka"],
+];
+
+echo json_encode($data);
+?>
